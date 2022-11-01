7bae1c3609aed2054693257729d3dfba7aae33e7
mi pece 01 <benjy_play@outlook.com>
2022-11-01 00:18:11 -0600

    creacion practica 3

---
+++ practicas/practica 3/CuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,23 @@
+public class CuentaBancaria {
+
+     public static void main(String[] args){
+
+           double saldo = 1000.75;
+           double cantidadARetirar= 250;
+	   double saldoActualizado = saldo - cantidadARetirar;
+	   double cantidadParaCadaAmigo = saldoActualizado / 3;
+	   boolean puedeComprarTicket = true;
+
+	   if (cantidadParaCadaAmigo >= 250){
+		System.out.println("¿Cada amigo puede comprar un ticket?" + puedeComprarTicket);
+	   }
+		
+	   else{
+		puedeComprarTicket = false;
+		System.out.println("¿Cada amigo puede comprar un ticket?" + puedeComprarTicket);			
+	   }
+	   
+	   System.out.println("Le di a cada amigo" + cantidadParaCadaAmigo);
+
+     }
+}
